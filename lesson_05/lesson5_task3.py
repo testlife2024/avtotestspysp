abe5e2c79904ef8bf32fde778cc656fd6ffc8b01
itisa228 <pulnovaalisa@gmail.com>
2025-05-20 09:35:48 +0300

    Update lesson5_task3.py

--- lesson_05/lesson5_task3.py
+++ lesson_05/lesson5_task3.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Firefox()
 driver.get("http://the-internet.herokuapp.com/inputs")
 driver.maximize_window()
-field = driver.find_element(By.CSS_SELECTOR,'#content > div > div > div > input[type=number]')
+field = driver.find_element(By.CSS_SELECTOR,'#content input[type=number]')
 field.send_keys('Sky')
 field.clear()
 field.send_keys('Pro')
